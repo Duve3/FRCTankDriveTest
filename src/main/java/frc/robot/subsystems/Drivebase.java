5ad6f28345418fc74c3db2a1d94a7bf639ffb20e
Dhruv Hooda <Duv3tabest@gmail.com>
2026-07-02 14:00:44 -0700

    fixed config issues, implemented simulations

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -3,64 +3,68 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.system.plant.DCMotor;
 
 import com.revrobotics.ResetMode;
-
+import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
 
-
 public class Drivebase extends SubsystemBase {
-  // The 4 motors of the base
+  
+  // 1. The 4 motors of the base (Always SparkMax, real or sim!)
   SparkMax frontLeftMotor = new SparkMax(1, MotorType.kBrushless);
   SparkMax backLeftMotor = new SparkMax(2, MotorType.kBrushless);
   SparkMax frontRightMotor = new SparkMax(3, MotorType.kBrushless);
   SparkMax backRightMotor = new SparkMax(4, MotorType.kBrushless);
 
+  // 2. Declare the simulation wrappers separately
+  SparkMaxSim frontLeftSim;
+  SparkMaxSim backLeftSim;
+  SparkMaxSim frontRightSim;
+  SparkMaxSim backRightSim;
+
   /** Creates a new Drivebase subsystem, configures motors */
   public Drivebase() {
-    // here we are defining configs and making them more and more exact as we define more
+    
+    // 3. Only initialize the wrappers if we are in Sim mode
+    if (RobotBase.isSimulation()) {
+        // You pass the real motor in, plus the physical gearbox/motor model (e.g., 1 NEO)
+        frontLeftSim = new SparkMaxSim(frontLeftMotor, DCMotor.getNEO(1));
+        backLeftSim = new SparkMaxSim(backLeftMotor, DCMotor.getNEO(1));
+        frontRightSim = new SparkMaxSim(frontRightMotor, DCMotor.getNEO(1));
+        backRightSim = new SparkMaxSim(backRightMotor, DCMotor.getNEO(1));
+    }
 
-    // Creates a configuration to apply to motors
+    // Creates a standard configuration
     SparkMaxConfig config = new SparkMaxConfig();
-    // Sets to brake when robot isn't given power
     config.idleMode(IdleMode.kBrake);
+    config.smartCurrentLimit(40); // 40A is a great, safe choice to chillax with!
 
-    // sets a current limit (default is 80, that feels quite high, CD recs 60 but im gonna chillax)
-    config.smartCurrentLimit(40);
+    // 4. FIX: Create a NEW config object for the inverted side
+    SparkMaxConfig invertConfig = new SparkMaxConfig();
+    invertConfig.apply(config);  // Copy the brake/current limit settings over
+    invertConfig.inverted(true); // Now invert ONLY this config
 
-    // Inverts direction (bc some of them need to be inverted)
-    SparkMaxConfig invertConfig = config;
-    invertConfig.inverted(true);
-
-    // Reset mode resets the already saved flash settings to default values, Persist mode saves current settings across cycles
+    // Apply the configurations
     frontLeftMotor.configure(invertConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-    backLeftMotor.configure(config.follow(frontLeftMotor), ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    backLeftMotor.configure(invertConfig.follow(frontLeftMotor), ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    
     frontRightMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
-    backRightMotor.configure(invertConfig.follow(frontRightMotor), ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    backRightMotor.configure(config.follow(frontRightMotor), ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-   /**
-   * Moves the robot using the power provided
-   * @param leftPower the power to put on left side
-   * @param rightPower the power to put on right side
-   * @return command
-   */
   public void move(double leftPower, double rightPower) {
     frontLeftMotor.set(leftPower);
     frontRightMotor.set(rightPower);
   }
 
-  /**
-   * Sets all motor power to 0, stopping them
-   * @return command
-   */
   public void stop() {
       frontLeftMotor.set(0);
       frontRightMotor.set(0);
